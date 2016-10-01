07234c83cea5fe9ad3f94727675645af8c9413e3
Kiko Beats <josefrancisco.verdu@gmail.com>
2016-10-01 21:02:58 +0200

    Add awesome icons

--- src/app/NavBar/index.js
+++ src/app/NavBar/index.js
@@ -1,11 +1,15 @@
 import React from 'react'
 
 import InputSearch from '../InputSearch'
+import IconMenu from 'react-icons/lib/md/menu'
+import IconFilter from 'react-icons/lib/md/filter-list'
 
 export default function NavBar ({children}) {
   return (
-    <nav className='bg-light-silver w-100 center pv3 flex justify-center'>
+    <nav className='bg-light-silver w-100 center pv3 flex justify-around items-center'>
+      <IconMenu />
       <InputSearch />
+      <IconFilter />
     </nav>
   )
 }
