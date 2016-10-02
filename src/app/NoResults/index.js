c36a8baf4ebd01930dff829fbe2de9dbac1f1d50
Kiko Beats <josefrancisco.verdu@gmail.com>
2016-10-02 18:20:53 +0200

    Minimal scaffold

--- src/app/NoResults/index.js
+++ src/app/NoResults/index.js
@@ -2,11 +2,11 @@ import React from 'react'
 
 function NoResults ({query}) {
   return (
-    <div className='results-wrapper'>
-      <div className='no-results'>
-        No results found matching <span className='query'>{query}</span>
-      </div>
-    </div>
+    <section className='ph3 ph7-l vh-100'>
+      <h1>
+        No results found matching for &#039;<span>{query}</span>&#039;.
+      </h1>
+    </section>
   )
 }
 
