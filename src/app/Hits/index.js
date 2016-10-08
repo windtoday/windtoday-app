5714fac95eed31e501d3c6b1954b913a00901491
Kiko Beats <josefrancisco.verdu@gmail.com>
2016-10-08 15:33:09 +0200

    Refactor Hits

--- src/app/Hits/index.js
+++ src/app/Hits/index.js
@@ -4,24 +4,55 @@ import {Hits} from 'react-instantsearch'
 import Pagination from '../Pagination'
 import Stats from '../Stats'
 
+function price (item) {
+  if (item.price) {
+    return (
+      <dl className='mt2 f6'>
+        <dt className='clip'>Price</dt>
+        <dd className='ml0'>{item.price}€</dd>
+      </dl>
+    )
+  }
+}
+
+function image (item) {
+  const link = `/assets/img/${item.provider}.jpeg`
+
+  return (
+    <div className='dtc w3'>
+      <img src={link} className='db w-100 br-100' />
+    </div>
+  )
+}
+
+function date (item) {
+  const locale = navigator.language
+
+  const opts = {
+    month: 'short',
+    day: 'numeric',
+    year: 'numeric'
+  }
+
+  const timestamp = item.updatedAt || item.createdAt
+  return (new Date(timestamp)).toLocaleString(locale, opts)
+}
+
 function Hit ({item}) {
   return (
     <section>
-      <a className='link dt w-100 bb b--black-10 pb2 mt2 dim blue' href='#0'>
-        <div className='dtc w3'>
-          <img src={item.image} className='db w-100' />
-        </div>
+      <a className='link dt w-100 bb b--black-10 pb2 mt2 dim blue' href={item.link}>
+
+        {image(item)}
+
         <div className='dtc v-top pl2'>
           <p className='f6 f5-ns fw6 lh-title black mv0' dangerouslySetInnerHTML={{
-            __html: item._highlightResult.name.value
+            __html: item._highlightResult.title.value
           }} />
-          <p className='f6 fw4 mt2 mb0 black-60' dangerouslySetInnerHTML={{
-            __html: item._highlightResult.type.value
-          }} />
-          <dl className='mt2 f6'>
-            <dt className='clip'>Price</dt>
-            <dd className='ml0'>${item.price}</dd>
-          </dl>
+          <p className='f6 fw4 mt2 mb0 black-60' >
+            {date(item)}
+          </p>
+          {price(item)}
         </div>
       </a>
     </section>
