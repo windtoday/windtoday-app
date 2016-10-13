9fc2de2bf78347f4677d826093ca0472660bc483
Kiko Beats <josefrancisco.verdu@gmail.com>
2016-10-13 10:05:52 +0200

    Refactor styles

--- src/app/CurrentFilters/index.js
+++ src/app/CurrentFilters/index.js
@@ -10,6 +10,13 @@ function CustomCurrentFilters () {
       translations={{
         clearFilter: <IconClose />
       }}
+
+      theme={{
+        clearAll: 'dn',
+        filter: 'dib br4 pa1 mv2 mr2 bg-near-white',
+        filterLabel: 'pl2 v-mid',
+        filterClear: 'ais-CurrentFilters__filterClear'
+      }}
     />
   )
 }
