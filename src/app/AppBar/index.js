1784de6720fcd21c2e547f646f7dcf86eaac5331
Kiko Beats <josefrancisco.verdu@gmail.com>
2016-10-15 23:17:20 +0200

    Add Logo

--- src/app/AppBar/index.js
+++ src/app/AppBar/index.js
@@ -3,16 +3,17 @@ import IconMenu from 'react-icons/lib/md/menu'
 import IconFilter from 'react-icons/lib/md/filter-list'
 
 import InputSearch from '../InputSearch'
+import Logo from '../Logo'
 import './style.scss'
 
 const iconStyle = 'dim pointer blue'
 
 function AppBar ({toggle, get}) {
   return (
-    <header data-app='app-bar' className='bg-white w-100 center pv3 flex justify-around items-center fixed z-1'>
+    <header data-app='app-bar' className='bg-white w-100 center pv3 flex justify-around items-center fixed z-1 ph4'>
       <IconMenu className={iconStyle} onClick={toggle('facetsOpen')} />
+      <Logo />
       <InputSearch />
-      <IconFilter className={iconStyle} />
     </header>
   )
 }
---
+++ src/app/Logo/index.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Logo () {
+  return (
+    <a href='#'>
+      <img src='/assets/img/logo.png' className='w5 dib-l dn' />
+    </a>
+  )
+}
+
+export default Logo
