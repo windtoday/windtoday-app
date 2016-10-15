68c8f7a071ebf13d1a93d477ab85c3cdb31cf8fa
Kiko Beats <josefrancisco.verdu@gmail.com>
2016-10-15 19:09:05 +0200

    Adjust icon style

--- src/app/AppBar/index.js
+++ src/app/AppBar/index.js
@@ -5,12 +5,14 @@ import IconFilter from 'react-icons/lib/md/filter-list'
 import InputSearch from '../InputSearch'
 import './style.scss'
 
+const iconStyle = 'dim pointer blue'
+
 function AppBar ({toggle, get}) {
   return (
     <header data-app='app-bar' className='bg-white w-100 center pv3 flex justify-around items-center fixed z-1'>
-      <IconMenu className='dim pointer' onClick={toggle('facetsOpen')} />
+      <IconMenu className={iconStyle} onClick={toggle('facetsOpen')} />
       <InputSearch />
-      <IconFilter />
+      <IconFilter className={iconStyle} />
     </header>
   )
 }
