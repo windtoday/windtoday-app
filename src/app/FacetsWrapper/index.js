dc6f6b90f61b3cec96840dba388429e32a9ba7c8
Kiko Beats <josefrancisco.verdu@gmail.com>
2016-10-16 01:40:30 +0200

    Adjust Range style

--- src/app/FacetsWrapper/index.js
+++ src/app/FacetsWrapper/index.js
@@ -4,7 +4,7 @@ import './style.scss'
 
 function SidebarSection ({title, item}) {
   return (
-    <article className='pa3 pa4-ns'>
+    <article data-app={`facet-${title}`} className='pa3 pa4-ns'>
       <div className='f6 fw6 ttu tracked pb2 bb b--black-10 black-70'>{title}</div>
       {item}
     </article>
@@ -80,10 +80,6 @@ function Facets () {
           <Range
             attributeName='size'
             id='s'
-            theme={Object.assign({}, Range.defaultTheme, {
-              boundMin: 'ais-Range__bound ais-Range__boundMin Range__size',
-              boundMax: 'ais-Range__bound ais-Range__boundMax Range__size'
-            })}
           />
         }
       />
@@ -94,10 +90,6 @@ function Facets () {
           <Range
             attributeName='litres'
             id='l'
-            theme={Object.assign({}, Range.defaultTheme, {
-              boundMin: 'ais-Range__bound ais-Range__boundMin Range__litres',
-              boundMax: 'ais-Range__bound ais-Range__boundMax Range__litres'
-            })}
           />
         }
       />
@@ -108,10 +100,6 @@ function Facets () {
           <Range
             attributeName='price'
             id='e'
-            theme={Object.assign({}, Range.defaultTheme, {
-              boundMin: 'ais-Range__bound ais-Range__boundMin Range__price',
-              boundMax: 'ais-Range__bound ais-Range__boundMax Range__price'
-            })}
           />
         }
       />
