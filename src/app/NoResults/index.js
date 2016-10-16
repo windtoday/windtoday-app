6b1b43773005b7c7fdc610db80e38f52e9682c24
Kiko Beats <josefrancisco.verdu@gmail.com>
2016-10-16 13:10:18 +0200

    Improve no results style

--- src/app/NoResults/index.js
+++ src/app/NoResults/index.js
@@ -2,9 +2,9 @@ import React from 'react'
 
 function NoResults ({query}) {
   return (
-    <article data-app='no-results' className='ph3 ph7-l vh-100'>
+    <article data-app='no-results' className='tc pv4-l pv3 vh-100'>
       <h1>
-        No results found matching for &#039;<span>{query}</span>&#039;.
+        No results found matching for <span className='blue'>{query}</span>.
       </h1>
     </article>
   )
