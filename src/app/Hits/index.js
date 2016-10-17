6e34f748864feb04a84ceb1e0d79c916fbaf1322
Kiko Beats <josefrancisco.verdu@gmail.com>
2016-10-17 20:53:06 +0200

    Hide pagination when is not necessary

--- src/app/Hits/index.js
+++ src/app/Hits/index.js
@@ -5,13 +5,14 @@ import Pagination from '../Pagination'
 import Stats from '../Stats'
 import Hit from '../Hit'
 
-function CustomHits ({hits}) {
+function CustomHits ({hits, hitsPerPage}) {
+  const hasPagination = hits.length > hitsPerPage
   return (
     <div data-app='hits'>
       <Stats />
       <div className='pv3 pv4-l ph3 ph5-l'>
         {hits.map((hit, idx) => <Hit item={hit} key={idx} />)}
-        <Pagination />
+        {hasPagination && <Pagination /> }
       </div>
     </div>
   )
