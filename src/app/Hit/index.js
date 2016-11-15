74e8cbb7728e27591326851bc0fb8882b9efdaad
Kiko Beats <josefrancisco.verdu@gmail.com>
2016-11-15 22:58:05 +0100

    WIP

--- src/app/Hit/index.js
+++ src/app/Hit/index.js
@@ -3,20 +3,14 @@ import React from 'react'
 import './style.scss'
 
 function price (item) {
-  const price = item.price ? `${item.price}€` : 'N/A'
-  return (
-    <dl className='mt0 f5 f4-ns'>
-      <dt className='clip'>Price</dt>
-      <dd className='ml0 f4'>{price}</dd>
-    </dl>
-  )
+  return item.price ? `€${item.price}` : 'N/A'
 }
 
 function image (item) {
   const imageURL = `/assets/img/provider/${item.provider}.jpg`
   return (
-    <div className='dtc w3 v-mid'>
-      <img src={imageURL} className='db w-80 w-100-ns br3' />
+    <div data-hit='image' className='dtc v-mid'>
+      <img src={imageURL} className='db w-100 br3' />
     </div>
   )
 }
@@ -36,22 +30,22 @@ function date (item) {
 
 function Hit ({item, toggle, get}) {
   return (
-    <article data-app='hit'>
-      <a className='link dt w-100 bb b--near-white pv3 blue' href={item.shortenUrl} target='_blank'>
+    <article data-app='hit' className='bb b--near-white lh-title'>
+      <a data-hit='link' className='link dt w-100 pv2 pv1-ns blue' href={item.shortenUrl} target='_blank'>
 
         {image(item)}
 
-        <div className='dtc v-top pl3-ns pl1'>
-          <p className='f6 f5-ns fw6 lh-title dark-blue mv0 hover-blue' dangerouslySetInnerHTML={{
-            __html: item._highlightResult.title.value
-          }} />
-          <p className='f6 fw4 mt2 mb0 black-60' >
-            {date(item)}
-          </p>
+        <div className='dtc f4 w3 v-mid tc'>
+          <p data-hit='price' className='dib ph2 ph3-ns'>{price(item)}</p>
         </div>
 
-        <div className='dtc v-top pl3 tr'>
-          {price(item)}
+        <div className='dtc v-mid pr5'>
+          <p data-hit='title' className='f6 f5-ns fw6 dark-blue mv0 hover-blue' dangerouslySetInnerHTML={{
+            __html: item._highlightResult.title.value
+          }} />
+          <p data-hit='date' className='f6 fw4 mt2 mb0 black-60' >
+            {date(item)}
+          </p>
         </div>
       </a>
     </article>
