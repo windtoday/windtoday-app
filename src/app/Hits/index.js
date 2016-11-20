34ea7a57a6aa68c4a3f5bba97a8bb5105957d06a
Kiko Beats <josefrancisco.verdu@gmail.com>
2016-11-20 20:43:20 +0100

    Refactor

--- src/app/Hits/index.js
+++ src/app/Hits/index.js
@@ -4,7 +4,7 @@ import Hit from '../Hit'
 
 import './style.scss'
 
-function CustomHits ({toggle, get, hits}) {
+function CustomHits ({toggle, get, hits, refine}) {
   const props = { toggle, get }
 
   return (
@@ -13,6 +13,13 @@ function CustomHits ({toggle, get, hits}) {
       <div className='pv3 pv4-l'>
         {hits.map((item, key) => <Hit item={item} key={key} {...props} />)}
       </div>
+      <footer className='tc pb3'>
+        <a
+          onClick={refine}
+          className='pointer f6 link br2 ba ph3 pv2 mb2 dib blue hover-bg-blue hover-white'>
+          Load more
+        </a>
+      </footer>
     </div>
   )
 }
--- src/app/Results/index.js
+++ src/app/Results/index.js
@@ -8,26 +8,8 @@ import './style.scss'
 
 const theme = 'fl w-100 bg-white vh-100 overflow-x-hidden overflow-y-scroll'
 
-const Hit = ({item, hasMore, refine}) => {
-  return (
-    <p>{item.title}</p>
-  )
-}
-
-function footer (refine) {
-  return (
-    <footer className='tc pv3 pv4-l'>
-      <a
-        onClick={refine}
-        className='pointer f6 link dim br2 ba ph3 pv2 mb2 dib blue'>
-        Load more
-      </a>
-    </footer>
-  )
-}
-
 function Results ({hasResults, query, hasMore, toggle, get, refine, hits}) {
-  const props = { toggle, get, hasMore, refine, hits }
+  const props = { toggle, get, hasMore, refine, hits, query }
 
   const style = classnames(theme, {
     'results-expand': get('facetsOpen')
@@ -35,8 +17,7 @@ function Results ({hasResults, query, hasMore, toggle, get, refine, hits}) {
 
   return (
     <section data-app='results' className={style}>
-      <Hits {...props} />
-      {hasMore && footer(refine)}
+      {hasResults ? <Hits {...props} /> : <NoHits {...props} />}
     </section>
   )
 }
