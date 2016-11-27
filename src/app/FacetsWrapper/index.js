dd7d226510cdcdc743d271138b168fb4380a3d8b
Kiko Beats <josefrancisco.verdu@gmail.com>
2016-11-27 01:46:42 +0100

    WIP

--- src/app/FacetsWrapper/index.js
+++ src/app/FacetsWrapper/index.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import RefinementList from '../RefinementList'
+import Range from '../Range'
 
 import './style.scss'
 
@@ -7,108 +8,7 @@ function Facets () {
   return (
     <div data-app='facets-wrapper'>
 
-      <RefinementList
-        title='price'
-        attributeName='price'
-        limitMin={5}
-        showMore />
-
-      <RefinementList
-        title='category'
-        attributeName='category' />
-
-      <RefinementList
-        title='provider'
-        attributeName='provider' />
-
-      <RefinementList
-        title='seller'
-        attributeName='seller' />
-
-      <RefinementList
-        title='brand'
-        attributeName='brand'
-        limitMin={5}
-        showMore />
-
-      <RefinementList
-        title='model'
-        attributeName='model'
-        limitMin={5}
-        showMore />
-
-      <RefinementList
-        title='sail size'
-        attributeName='sail.size'
-        limitMin={5}
-        showMore />
-
-      <RefinementList
-        title='board size'
-        attributeName='board.size'
-        limitMin={5}
-        showMore />
-
-      <RefinementList
-        title='mast type'
-        limitMin={5}
-        attributeName='mast.type'
-        showMore />
-
-      <RefinementList
-        title='mast carbon'
-        limitMin={5}
-        attributeName='mast.carbon'
-        showMore />
-
-      <RefinementList
-        title='mast size'
-        attributeName='mast.size'
-        limitMin={5}
-        showMore />
-
-      <RefinementList
-        title='fin size'
-        attributeName='fin.size'
-        limitMin={5}
-        showMore />
-
-      <RefinementList
-        title='fin type'
-        attributeName='fin.type'
-        limitMin={5}
-        showMore />
-
-      <RefinementList
-        title='mast type'
-        attributeName='mast.type'
-        limitMin={5}
-        showMore />
-
-      <RefinementList
-        title='mast carbon'
-        attributeName='mast.carbon'
-        limitMin={5}
-        showMore />
-
-      <RefinementList
-        title='mast size'
-        attributeName='mast.size'
-        limitMin={5}
-        showMore />
-
-      <RefinementList
-        title='boom type'
-        attributeName='boom.type'
-        limitMin={5}
-        showMore />
-
-      <RefinementList
-        title='boom size'
-        attributeName='boom.size'
-        limitMin={5}
-        showMore />
-
+      <Range attributeName='price' />
     </div>
   )
 }
