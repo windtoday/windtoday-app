3762e7a6f5edd27e23e0ff3bc7a3fec8b344ac3a
Kiko Beats <josefrancisco.verdu@gmail.com>
2016-11-27 21:14:27 +0100

    Conditional footer

--- src/app/Hits/index.js
+++ src/app/Hits/index.js
@@ -4,22 +4,29 @@ import Hit from '../Hit'
 
 import './style.scss'
 
-function CustomHits ({toggle, get, hits, refine}) {
-  const props = { toggle, get }
+function renderHasMore (refine) {
+  return (
+    <footer className='tc pv3'>
+      <a
+        onClick={refine}
+        className='pointer f6 link br2 ba ph3 pv2 mb2 dib blue hover-bg-blue hover-white'>
+        Load more
+      </a>
+    </footer>
+  )
+}
+
+function CustomHits (props) {
+  const {toggle, get, hits, refine, hasMore} = props
+  const _props = { toggle, get }
 
   return (
     <div data-app='hits' className='ph3 ph4-ns'>
       <Stats />
       <div className='pv3'>
-        {hits.map((item, key) => <Hit item={item} key={key} {...props} />)}
+        {hits.map((item, key) => <Hit item={item} key={key} {..._props} />)}
       </div>
-      <footer className='tc pv3'>
-        <a
-          onClick={refine}
-          className='pointer f6 link br2 ba ph3 pv2 mb2 dib blue hover-bg-blue hover-white'>
-          Load more
-        </a>
-      </footer>
+      {hasMore && renderHasMore(refine)}
     </div>
   )
 }
