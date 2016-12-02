5f64455f145338ab1b953648079f95d25bd3c810
Kiko Beats <josefrancisco.verdu@gmail.com>
2016-12-02 14:39:19 +0100

    Improve bar color

--- src/app/AppBar/index.js
+++ src/app/AppBar/index.js
@@ -1,17 +1,26 @@
 import React from 'react'
 import IconMenu from 'react-icons/lib/md/menu'
+import IconClose from 'react-icons/lib/md/close'
 
 import SearchBox from '../SearchBox'
 import Logo from '../Logo'
 
-const iconStyle = 'dim pointer blue'
+import './style.scss'
+
+const renderMenuButton = (props) => <IconMenu {...props} />
+const renderCloseButton = (props) => <IconClose {...props} />
 
 function AppBar (props) {
-  const {toggle} = props
+  const {toggle, get} = props
+  const renderButton = get('facetsOpen') ? renderCloseButton : renderMenuButton
 
   return (
-    <header data-app='app-bar' className='bg-navy w-100 center pb2 pt1 ph1 flex justify-around items-center fixed z-max'>
-      <IconMenu className={iconStyle} onClick={toggle('facetsOpen')} />
+    <header data-app='app-bar' className='w-100 center pb2 pt1 ph1 flex justify-around items-center fixed z-max'>
+      {renderButton({
+        className: 'dim pointer blue',
+        onClick: toggle('facetsOpen')
+      })}
+
       <Logo {...props} />
       <SearchBox {...props} />
     </header>
