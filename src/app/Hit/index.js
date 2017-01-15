5058dff608e72a5fa09b2e9917b16319bffffe78
Kiko Beats <josefrancisco.verdu@gmail.com>
2017-01-15 21:46:54 +0100

    Fix linter

--- src/app/Hit/index.js
+++ src/app/Hit/index.js
@@ -53,7 +53,7 @@ const Hit = createClass({
 
   render () {
     const {props, state, onHover} = this
-    const {item, toggle, get} = props
+    const {item} = props
     const {isHover} = state
 
     const titleStyle = classnames('link f6 f5-l fw6 mv0 helvetica navy', {
