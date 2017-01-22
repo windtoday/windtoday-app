5491abe8f11732ef74956adc3368ae8a9025777f
Kiko Beats <josefrancisco.verdu@gmail.com>
2017-01-22 13:18:20 +0100

    Setup aside right icon

--- src/app/AppBar/index.js
+++ src/app/AppBar/index.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import IconMenu from 'react-icons/lib/md/menu'
 import IconClose from 'react-icons/lib/md/close'
+import IconFilter from 'react-icons/lib/md/filter-list'
 
 import SearchBox from '../SearchBox'
 import Logo from '../Logo'
@@ -8,23 +9,32 @@ import Logo from '../Logo'
 import './style.scss'
 
 const renderMenuButton = (props) => <IconMenu {...props} />
+const renderFilterButton = (props) => <IconFilter {...props} />
 const renderCloseButton = (props) => <IconClose {...props} />
 
 function AppBar (props) {
   const {toggle, get} = props
-  const renderButton = get('asideLeftOpen') ? renderCloseButton : renderMenuButton
+  const asideLeftButton = get('asideLeftOpen') ? renderCloseButton : renderMenuButton
+  const asideRightButton = get('asideRightOpen') ? renderCloseButton : renderFilterButton
 
   return (
     <header
       data-app='app-bar'
       className='AppBar bg-white w-100 center pb2 pt1 ph1 flex justify-around items-center fixed z-2'>
-      {renderButton({
+
+      {asideLeftButton({
         className: 'dim pointer blue',
         onClick: toggle('asideLeftOpen')
       })}
 
       <Logo {...props} />
       <SearchBox {...props} />
+
+      {asideRightButton({
+        className: 'dim pointer blue',
+        onClick: toggle('asideRightOpen')
+      })}
+
     </header>
   )
 }
--- src/app/AsideLeft/index.js
+++ src/app/AsideLeft/index.js
@@ -6,14 +6,16 @@ import './style.scss'
 const theme = 'AsideLeft fl w-100 w-25-l vh-100 bg-white overflow-x-hidden overflow-y-scroll z-1'
 
 function AsideLeft (props) {
-  const { get } = props
+  const { get, children } = props
 
   const style = classnames(theme, {
     'AsideLeft__collapse': !get('asideLeftOpen')
   })
 
   return (
-    <aside data-app='aside-left' className={style} {...props} />
+    <aside data-app='aside-left' className={style}>
+      {children}
+    </aside>
   )
 }
 
