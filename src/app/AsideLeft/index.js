28a8805bd0c389b75d434790336a407cc235a99e
Kiko Beats <josefrancisco.verdu@gmail.com>
2017-01-22 12:54:46 +0100

    Remove console.log

--- src/app/AsideLeft/index.js
+++ src/app/AsideLeft/index.js
@@ -6,7 +6,6 @@ import './style.scss'
 const theme = 'AsideLeft fl w-100 w-25-l vh-100 bg-white overflow-x-hidden overflow-y-scroll z-1'
 
 function AsideLeft (props) {
-  console.log(props)
   const { get } = props
 
   const style = classnames(theme, {
