b10de9eaadb64b8bf749d0b928abe6c6700c0a6b
Kiko Beats <josefrancisco.verdu@gmail.com>
2017-01-26 19:54:29 +0100

    Fix #67

--- src/app/CurrentRefinements/index.js
+++ src/app/CurrentRefinements/index.js
@@ -4,10 +4,36 @@ import IconClose from 'react-icons/lib/md/close'
 
 import './style.scss'
 
+const isRange = (item) => item.currentRefinement.max && item.currentRefinement.min
+const isDottedLabel = item => item.label.indexOf('.') !== -1
+
+function transformDotLabel (item) {
+  const {label: itemLabel} = item
+  const label = itemLabel.replace('.', ' ')
+  return Object.assign(item, {label})
+}
+
+function transformRange (item) {
+  return {
+    currentRefinement: item.currentRefinement,
+    label: `${item.attributeName}: `,
+    items: [{'label': item.label, value: item.value}]
+  }
+}
+
+function transformItems (items) {
+  return items.map(function (item) {
+    if (isRange(item)) item = transformRange(item)
+    if (isDottedLabel(item)) item = transformDotLabel(item)
+    return item
+  })
+}
+
 function CustomCurrentRefinements () {
   return (
     <div data-app='current-filters' >
       <CurrentRefinements
+        transformItems={transformItems}
         translations={{
           clearFilter: <IconClose style={{verticalAlign: 'baseline'}} />
         }}
