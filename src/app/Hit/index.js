2b195200c40068150513bdcf3640974a3e2d6811
Kiko Beats <josefrancisco.verdu@gmail.com>
2017-05-02 22:06:44 +0200

    Be possible highlight provider

--- src/app/Hit/index.js
+++ src/app/Hit/index.js
@@ -16,13 +16,12 @@ function getTimestamp (item) {
   return item.updatedAt || item.createdAt
 }
 
-export default (props) => {
+export default props => {
   const {item} = props
   const {provider, price, image} = item
 
   const imageURL = image || `/assets/img/provider/${provider}.jpg`
   const priceText = price ? `${price}€` : 'N/A'
-  const providerText = `by ${provider}`
 
   const timestamp = getTimestamp(item)
 
@@ -30,14 +29,12 @@ export default (props) => {
     <article
       data-app='hit'
       role='article'
-      className='hit fade-in bg-white pa3 bb b--light-gray h6'
-      >
+      className='hit fade-in bg-white pa3 bb b--light-gray h6'>
       <a
         className='hit__link flex link w-100 h-100 black'
         href={item.link}
         target='_blank'
-        rel='nofollow noopener'
-        >
+        rel='nofollow noopener'>
 
         <div className='w-100 lh-copy f4'>
           <p className='link fw4 lh-title mv0 blue-grey-700 w-95 pb1'>
@@ -46,10 +43,11 @@ export default (props) => {
           </p>
 
           <p className='ma0'>
-            <span
-              className='cyan-500 b pr1'>{priceText}
-            </span> <span
-              className='tracked blue-grey-200 f6'>{providerText}</span>
+            <span className='cyan-500 b pr1'>{priceText}</span>
+            {' '}
+            <div className='tracked blue-grey-200 f6 di'>
+              by <Highlight attributeName='provider' hit={item} />
+            </div>
           </p>
 
         </div>
