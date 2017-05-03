cac84904cc9a5d1a11cd9efca4f600689f854f01
Kiko Beats <josefrancisco.verdu@gmail.com>
2017-05-03 12:31:04 +0200

    Add highlight

--- src/app/NoHits/index.js
+++ src/app/NoHits/index.js
@@ -9,8 +9,12 @@ function NoHits (props) {
   return (
     <article data-app='no-hits' className='no-hits tc pa4-l pa4'>
       <IconWindFlagDown className='w4 pv4' />
-      <h1 className='no-break blue pt2'>{query}</h1>
-      <h2 className='mid-gray'>has not been seen in the last days.</h2>
+      <h1 className='no-break blue-500 f3'>
+        <em>{query}</em>
+      </h1>
+      <h2 className='ph4 blue-grey-500 f3'>
+        has not been seen in the last days.
+      </h2>
     </article>
   )
 }
