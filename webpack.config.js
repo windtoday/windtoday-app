c706e1e1ef1288568aaa4ac26863b094dc79bc89
Kiko Beats <josefrancisco.verdu@gmail.com>
2017-05-17 19:05:32 +0200

    Update load sorcemap

--- webpack.config.js
+++ webpack.config.js
@@ -55,7 +55,7 @@ module.exports = {
           loader: 'postcss-loader',
           options: {
             parser: 'postcss-scss',
-            sourceMap: 'inline'
+            sourceMap: true
           }
         }, {
           loader: 'sass-loader',
