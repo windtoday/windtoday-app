1a42a7cd8228196f17469cfe220868cbbfe1d67c
Kiko Beats <josefrancisco.verdu@gmail.com>
2017-05-28 13:27:03 +0200

    Improve result loader

--- src/app/Results/loader.js
+++ src/app/Results/loader.js
@@ -1,11 +1,35 @@
 import React from 'react'
 
+import IconWindtoday from '../Icon/windtoday'
+
 import Spinner from '../Spinner'
+
+const phrases = [
+  'applying enough sun cream',
+  'correcting boom height position',
+  'rigging the sail correctly'
+]
+
+const randomElement = phrases => (
+  phrases[Math.floor(Math.random() * phrases.length)]
+)
+
+const phrase = randomElement(phrases)
 
 export default () => (
   <article className='z-max fixed vh-100 dt w-100 bg-blue-500'>
     <div className='dtc v-mid tc'>
-      <Spinner />
+      <div>
+        <IconWindtoday className='w4' style={{fill: 'white'}} />
+      </div>
+
+      <div className='pv3 white fw3'>
+        <p className='f4'>{phrase}</p>
+      </div>
+
+      <div>
+        <Spinner size='20px' />
+      </div>
     </div>
   </article>
 )
