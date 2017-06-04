bf9575f61635174bb2024b6262217b7c897b9149
Kiko Beats <josefrancisco.verdu@gmail.com>
2017-06-04 13:52:07 +0200

    Star rating (#240)
    * Remove lock
    
    * Ensure center vertically
    
    * Refactor
    
    * Remove unnecessary icons
    
    * Add more context badges
    
    * starRating → priceScore

--- src/app/AppBar/index.js
+++ src/app/AppBar/index.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, {createElement} from 'react'
 import IconMenu from 'react-icons/lib/md/menu'
 import IconClose from 'react-icons/lib/md/close'
 import IconFilter from 'react-icons/lib/md/filter-list'
@@ -8,14 +8,21 @@ import Logo from './Logo'
 
 import './style.scss'
 
-const renderMenuButton = (props) => <IconMenu size={20} {...props} />
-const renderFilterButton = (props) => <IconFilter size={20} {...props} />
-const renderCloseButton = (props) => <IconClose size={20} {...props} />
+const ICON_SIZE = 20
+
+const renderIcon = IconComponent => props =>
+  createElement(IconComponent, {...props, size: ICON_SIZE})
 
 function AppBar (props) {
   const {toggle, get} = props
-  const asideLeftButton = get('asideLeftOpen') ? renderCloseButton : renderMenuButton
-  const asideRightButton = get('asideRightOpen') ? renderCloseButton : renderFilterButton
+
+  const asideLeftButton = get('asideLeftOpen')
+  ? renderIcon(IconClose)
+  : renderIcon(IconMenu)
+
+  const asideRightButton = get('asideRightOpen')
+  ? renderIcon(IconClose)
+  : renderIcon(IconFilter)
 
   return (
     <header
@@ -23,7 +30,9 @@ function AppBar (props) {
       data-app='appbar'
       className='appbar bg-blue-700 w-100 ph3 ph5-ns flex justify-around items-center fixed z-5 card-shadow'>
 
-      <div className='order-1 pa0 ma0 flex justify-between' style={{flexGrow: 1}}>
+      <div
+        className='order-1 pa0 ma0 flex justify-between items-center'
+        style={{flexGrow: 1}}>
         {asideLeftButton({
           className: 'dim pointer white pr3',
           onClick: toggle('asideLeftOpen')
@@ -34,12 +43,13 @@ function AppBar (props) {
 
       <SearchBox className='order-2 mh3' {...props} />
 
-      <div className='order-3 pa0 flex justify-between' style={{flexGrow: 1}}>
+      <div
+        className='order-3 pa0 flex justify-between items-center'
+        style={{flexGrow: 1}}>
         {asideRightButton({
           className: 'dim pointer white pl3',
           onClick: toggle('asideRightOpen')
         })}
-
       </div>
 
     </header>
--- src/app/Icon/uncommon.js
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-
-export default props => (
-  <svg {...props} xmlns='http://www.w3.org/2000/svg' width='7' height='13'>
-    <path fill='#8696DD' d='M3.5 12C1.5 12 0 10.7 0 8.7S1.7 2.4 5 0c.6-.5-2 3.7 0 5 1.8 1.5 2 2.6 2 3.6 0 2-1.6 3.5-3.5 3.5z' />
-  </svg>
-)
