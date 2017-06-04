ef11b262aa4ebd8034656bde36628b45c3dd95d6
Kiko Beats <josefrancisco.verdu@gmail.com>
2017-06-04 13:48:54 +0200

    Remove unnecessary icons

--- src/app/Icon/rare.js
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-
-export default props => (
-  <svg {...props} xmlns='http://www.w3.org/2000/svg' width='8' height='14'>
-    <path fill='#A27EC0' d='M3.3 13.8c-2-.4-3.3-2-3.3-4 0-2.2 2-7 5.6-9.7.8-.6-2 4.2 0 5.8 2 1.6 2.4 3 2.4 4 0 1.4-.6 2.6-1.6 3.3 0-1-1-2.8-2.3-4C4 9 5 11 4 11.5c-.8.7-1 1.2-1 1.6 0 .3.2.6.3.8z' />
-  </svg>
-)
