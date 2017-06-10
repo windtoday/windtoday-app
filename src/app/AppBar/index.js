4c4ae666e107beda214d391b5b184924825fbf60
Kiko Beats <josefrancisco.verdu@gmail.com>
2017-06-10 12:32:04 +0200

    Setup appbar style

--- src/app/AppBar/index.js
+++ src/app/AppBar/index.js
@@ -1,9 +1,9 @@
-
-import React, {createElement} from 'react'
+import React, {createClass, createElement} from 'react'
 import IconMenu from 'react-icons/lib/md/menu'
 import IconClose from 'react-icons/lib/md/close'
 import IconFilter from 'react-icons/lib/md/filter-list'
 import classnames from 'classnames'
+import Headroom from 'react-headroom'
 
 import SearchBox from '../SearchBox'
 import Logo from './Logo'
@@ -15,72 +15,93 @@ const ICON_SIZE = 20
 const renderIcon = IconComponent => props =>
   createElement(IconComponent, {...props, size: ICON_SIZE})
 
-function AppBar (props) {
-  const {toggle, get} = props
+const AppBar = createClass({
+  getInitialState () {
+    return {
+      scroll: window.pageYOffset
+    }
+  },
+  componentDidMount () {
+    window.addEventListener('scroll', () => {
+      this.setState({scroll: window.pageYOffset})
+    })
+  },
+  render () {
+    const {scroll} = this.state
+    const {toggle, get} = this.props
 
-  const isSearching = get('isSearching')
+    const isSearching = get('isSearching')
 
-  const asideLeftButton = get('asideLeftOpen')
-  ? renderIcon(IconClose)
-  : renderIcon(IconMenu)
+    const asideLeftButton = get('asideLeftOpen')
+    ? renderIcon(IconClose)
+    : renderIcon(IconMenu)
 
-  const asideRightButton = get('asideRightOpen')
-  ? renderIcon(IconClose)
-  : renderIcon(IconFilter)
+    const asideRightButton = get('asideRightOpen')
+    ? renderIcon(IconClose)
+    : renderIcon(IconFilter)
 
-  return (
-    <header data-app='appbar' className='appbar fixed z-5 w-100'>
+    return (
+      <header className='bg-blue-500'>
+        <Headroom
+          data-app='appbar'
+          style={{
+            backgroundColor: scroll > 1 && '#424242'
+          }}
+          >
 
-      <div className='appbar__topbar flex justify-around items-center ph3 ph5-ns'>
-        <div
-          className={classnames('order-1 pa0 ma0 flex items-center', {
-            'justify-between': isSearching,
-            'justify-end': !isSearching
-          })}
+          <div className='appbar__topbar flex justify-around items-center ph3 ph5-ns'>
+            <div
+              className={classnames('order-1 pa0 ma0 flex items-center', {
+                'justify-between': isSearching,
+                'justify-end': !isSearching
+              })}
 
-          style={{flexGrow: 1}}>
-          {isSearching && asideLeftButton({
-            className: 'dim pointer white pr3',
-            onClick: toggle('asideLeftOpen')
-          })}
+              style={{flexGrow: 1}}>
+              {isSearching && asideLeftButton({
+                className: 'dim pointer white pr3',
+                onClick: toggle('asideLeftOpen')
+              })}
 
-          <Logo {...props} clearsQuery />
-        </div>
+              <Logo {...this.props} clearsQuery />
+            </div>
 
-        <SearchBox className='order-2 mh3' {...props} />
+            <SearchBox className='order-2 mh3' {...this.props} />
 
-        <div
-          className='order-3 pa0 flex justify-between items-center'
-          style={{flexGrow: 1}}>
-          {isSearching && asideRightButton({
-            className: 'dim pointer white pl3',
-            onClick: toggle('asideRightOpen')
-          })}
-        </div>
-      </div>
+            <div
+              className='order-3 pa0 flex justify-between items-center'
+              style={{flexGrow: 1}}>
+              {isSearching && asideRightButton({
+                className: 'dim pointer white pl3',
+                onClick: toggle('asideRightOpen')
+              })}
+            </div>
+          </div>
 
-      <nav className='appbar__navbar flex justify-center'>
-        <ul className='appbar__navbar-list list flex'>
-          <li className='appbar__navbar-item appbar__navbar-item--active'>
-            <a className='appbar__navbar-link link flex ttu b items-center justify-center flex-row h-100 ph3 blue-300 f6' href='/'>Home</a>
-          </li>
-          <li className='appbar__navbar-item relative'>
-            <a className='appbar__navbar-link link flex ttu b items-center justify-center flex-row h-100 ph3 blue-300 f6' href='/'>Categories</a>
-          </li>
-          <li className='appbar__navbar-item'>
-            <a className='appbar__navbar-link link flex ttu b items-center justify-center flex-row h-100 ph3 blue-300 f6' href='/'>Insights</a>
-          </li>
-          <li className='appbar__navbar-item'>
-            <a className='appbar__navbar-link link flex ttu b items-center justify-center flex-row h-100 ph3 blue-300 f6' href='/'>Blog</a>
-          </li>
-          <li className='appbar__navbar-item'>
-            <a className='appbar__navbar-link link flex ttu b items-center justify-center flex-row h-100 ph3 blue-300 f6' href='/'>About</a>
-          </li>
-        </ul>
-      </nav>
+          <nav className='appbar__navbar flex justify-center'>
+            <ul className='appbar__navbar-list list flex'>
+              <li className='appbar__navbar-item appbar__navbar-item--active'>
+                <a className='appbar__navbar-link link flex ttu b items-center justify-center flex-row h-100 ph3 blue-300 f6' href='/'>Home</a>
+              </li>
+              <li className='appbar__navbar-item relative'>
+                <a className='appbar__navbar-link link flex ttu b items-center justify-center flex-row h-100 ph3 blue-300 f6' href='/'>Categories</a>
+              </li>
+              <li className='appbar__navbar-item'>
+                <a className='appbar__navbar-link link flex ttu b items-center justify-center flex-row h-100 ph3 blue-300 f6' href='/'>Insights</a>
+              </li>
+              <li className='appbar__navbar-item'>
+                <a className='appbar__navbar-link link flex ttu b items-center justify-center flex-row h-100 ph3 blue-300 f6' href='/'>Blog</a>
+              </li>
+              <li className='appbar__navbar-item'>
+                <a className='appbar__navbar-link link flex ttu b items-center justify-center flex-row h-100 ph3 blue-300 f6' href='/'>About</a>
+              </li>
+            </ul>
+          </nav>
 
-    </header>
-  )
-}
+        </Headroom>
+      </header>
+    )
+  }
+
+})
 
 export default AppBar
--- src/app/Home/Header.js
+++ src/app/Home/Header.js
@@ -1,10 +1,10 @@
 import React from 'react'
 
 export default () => (
-  <article className='home w-100 bg-blue-500 white ph7'>
+  <article className='home w-100 bg-blue-500 white ph7 pt4 pb3'>
     <div className='pb4'>
       <p className='f2 tc measuse lh-copy avenir fw3 ma0 pv2'>
-        Daily windsurfing offers
+        windsurf marketplace
       </p>
       <p className='f4 tc measuse lh-copy ph5'>
         windtoday is a <strong>windsurfing marketplace</strong> to discover, compare & choose daily equipment offers. 💨
