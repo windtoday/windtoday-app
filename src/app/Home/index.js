7735f50a57e62a6b6a819d73d72c3a47d4de6fdb
Kiko Beats <josefrancisco.verdu@gmail.com>
2017-06-11 13:33:54 +0200

    Make search configurable

--- src/app/Home/index.js
+++ src/app/Home/index.js
@@ -1,4 +1,5 @@
 import React from 'react'
+
 import './style.scss'
 
 import HomeProducts from '../HomeProducts'
@@ -8,7 +9,21 @@ import Footer from './Footer'
 export default () => (
   <div>
     <Header />
-    <HomeProducts />
+
+    <HomeProducts
+      title='Feature Sails'
+      subtitle='View more sails →'
+      filters='category:sails AND provider:lpwind'
+      hitsPerPage={3}
+    />
+
+    <HomeProducts
+      title='Feature Sails'
+      subtitle='View more sails →'
+      filters='category:sails AND provider:wewind'
+      hitsPerPage={3}
+    />
+
     <Footer />
   </div>
 )
--- src/app/HomeProducts/index.js
+++ src/app/HomeProducts/index.js
@@ -1,26 +1,30 @@
 import React from 'react'
 import { connectHits } from 'react-instantsearch/connectors'
+import { Configure } from 'react-instantsearch/dom'
 
 import ProductCard from '../ProductCard'
 
-const RenderHits = ({hits}) => {
-  const products = hits.slice(0, 3)
-
+const RenderHits = ({hits: products, title, subtitle}) => {
   return (
     <article className='pv4 w-100 bg-white ph6'>
       <div className='f2 fw6 pt3 pb2 bb b--blue-500 black ttc bw2 flex justify-between items-center'>
-        <h3 className='ma0 avenir'>Feature Products</h3>
-        <a href='/pens/' className='link f5 blue-500 avenir'>View More Products →</a>
+        <h3 className='ma0 avenir'>{title}</h3>
+        <a href='/pens/' className='link f5 blue-500 avenir'>{subtitle}</a>
       </div>
       <div className='pt3 flex justify-around'>
-        {products.map((product, index) => <ProductCard key={index} product={product} />)}
+        {products.map((product, index) =>
+          <ProductCard key={index} product={product} />
+        )}
       </div>
     </article>
   )
 }
 
-const ConnectedRenderHits = connectHits(RenderHits)
+const HomeProducts = connectHits(RenderHits)
 
-export default () => (
-  <ConnectedRenderHits />
+export default ({title, filters, hitsPerPage}) => (
+  <div>
+    <Configure filters={filters} hitsPerPage={hitsPerPage} />
+    <HomeProducts title={title} />
+  </div>
 )
