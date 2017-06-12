d8598ba1c2ee2e8fc34b939502d72d5be08795d1
Kiko Beats <josefrancisco.verdu@gmail.com>
2017-06-12 23:59:56 +0200

    Tweaks

--- src/app/HomeProducts/index.js
+++ src/app/HomeProducts/index.js
@@ -2,16 +2,16 @@ import React from 'react'
 import { connectHits } from 'react-instantsearch/connectors'
 import { Configure, InstantSearch } from 'react-instantsearch/dom'
 
-import ProductGrid from '../ProductGrid'
+import ProductsGrid from '../ProductsGrid'
 
 const RenderHits = ({hits: products, title, subtitle}) => {
   return (
-    <section className='w-100 bg-white'>
+    <section className='w-100 bg-grey-50'>
       <div className='pv4 ph3 f4 f3-ns fw3 avenir black ttc flex justify-between items-center'>
         <h3 className='ma0 avenir'>{title}</h3>
         <a href='/pens/' className='link f5 blue-500 avenir'>{subtitle}</a>
       </div>
-      <ProductGrid products={products} />
+      <ProductsGrid products={products} />
     </section>
   )
 }
