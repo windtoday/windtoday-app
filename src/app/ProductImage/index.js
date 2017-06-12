8f1e995c3047c401d9a5524dba871aae0648bdae
Kiko Beats <josefrancisco.verdu@gmail.com>
2017-06-12 23:59:40 +0200

    Extract get image url

--- src/app/ProductImage/index.js
+++ src/app/ProductImage/index.js
@@ -1,20 +1,5 @@
 import React from 'react'
-
-const createImg = href => {
-  const el = document.createElement('a')
-  el.href = href
-  return el
-}
-
-function getImageUrl (product, width) {
-  const {image, provider} = product
-  if (!image) return `/assets/img/provider/${provider}.jpg`
-
-  const {hostname, pathname} = createImg(image)
-  const baseUrl = `https://images.weserv.nl/?url=${hostname}${pathname}`
-
-  return width ? `${baseUrl}&w=${width}` : baseUrl
-}
+import getImageUrl from '../util/get-image-url'
 
 export default ({product, className, width}) => (
   <img alt={product.title} src={getImageUrl(product, width)} className={className} />
---
+++ src/app/util/get-image-url.js
@@ -0,0 +1,16 @@
+const createImg = href => {
+  const el = document.createElement('a')
+  el.href = href
+  return el
+}
+
+function getImageUrl (product, width) {
+  const {image, provider} = product
+  if (!image) return `/assets/img/provider/${provider}.jpg`
+
+  const {hostname, pathname} = createImg(image)
+  const baseUrl = `https://images.weserv.nl/?url=${hostname}${pathname}`
+  return width ? `${baseUrl}&w=${width}` : baseUrl
+}
+
+export default getImageUrl
