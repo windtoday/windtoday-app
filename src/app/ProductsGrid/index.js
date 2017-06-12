f5389b21a696b897b2484666a5d6dd8b2b187415
Kiko Beats <josefrancisco.verdu@gmail.com>
2017-06-12 23:59:28 +0200

    Rename

--- src/app/ProductsGrid/index.js
+++ src/app/ProductsGrid/index.js
@@ -1,22 +1,22 @@
 import React from 'react'
 
 import './style.scss'
-
-import ProductImage from '../ProductImage'
+import ProductDivImage from '../ProductDivImage'
 
 const Product = (product) => {
   const {title, provider, link, price} = product
 
   return (
     <li className='mdc-grid-tile'>
-      <div className='mdc-grid-tile__primary'>
+      <div className='mdc-grid-tile__primary br3'>
         <a href={link} target='_blank'>
-          <ProductImage
-            className='mdc-grid-tile__primary-content'
-            product={product} />
+          <ProductDivImage
+            className='mdc-grid-tile__primary-content br3'
+            product={product}
+            />
         </a>
       </div>
-      <span className='mdc-grid-tile__secondary'>
+      <span className='mdc-grid-tile__secondary br3'>
         <i className='mdc-grid-tile__icon material-icons'>star_border</i>
         <span className='mdc-grid-tile__title'>{title}</span>
         <span className='mdc-grid-tile__support-text'>{price}€ by {provider}</span>
@@ -27,7 +27,7 @@ const Product = (product) => {
 
 export default ({products}) => {
   return (
-    <div className='mdc-grid-list mdc-grid-list--twoline-caption mdc-grid-list--tile-gutter-1'>
+    <div className='mdc-grid-list mdc-grid-list--twoline-caption'>
       <ul className='mdc-grid-list__tiles justify-center'>
         {products.map((props, index) => <Product key={index} {...props} />)}
       </ul>
