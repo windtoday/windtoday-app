e1a8213481793f4137a343576f59beb47b48e272
Kiko Beats <josefrancisco.verdu@gmail.com>
2017-06-12 23:59:48 +0200

    Add ProductDivImage

---
+++ src/app/ProductDivImage/index.js
@@ -0,0 +1,23 @@
+import React from 'react'
+import getImageUrl from '../util/get-image-url'
+
+export default ({product, className, width}) => {
+  const src = getImageUrl(product, width)
+
+  const linearGradient = `linear-gradient(
+    rgba(144, 158, 165, 0),
+    rgba(144, 158, 165,0.7),
+    rgb(144, 158, 165)
+  )`
+
+  const style = {
+    background: `${linearGradient}, url("${src}") center center / cover no-repeat`
+  }
+
+  return (
+    <div
+      className={className}
+      style={style}
+      />
+  )
+}
