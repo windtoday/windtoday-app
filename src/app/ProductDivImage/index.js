b36539a8af6c40c163f643d4fa1a52f8d4f65655
Kiko Beats <josefrancisco.verdu@gmail.com>
2017-06-14 14:06:42 +0200

    Allow children prop

--- src/app/ProductDivImage/index.js
+++ src/app/ProductDivImage/index.js
@@ -1,23 +1,23 @@
 import React from 'react'
 import getImageUrl from '../util/get-image-url'
 
-export default ({product, className, width}) => {
+export default ({product, className, width, children}) => {
   const src = getImageUrl(product, width)
 
   const linearGradient = `linear-gradient(
-    rgba(144, 158, 165, 0),
-    rgba(144, 158, 165,0.7),
-    rgb(144, 158, 165)
+    rgba(255, 255, 255, 0.1) 0%,
+    rgba(0, 0, 0, 0.6) 100%
   )`
 
   const style = {
-    background: `url("${src}") center center / cover no-repeat`
+    background: `${linearGradient}, url("${src}") center center / cover no-repeat`
   }
 
   return (
     <div
       className={className}
-      style={style}
-      />
+      style={style}>
+      {children}
+    </div>
   )
 }
