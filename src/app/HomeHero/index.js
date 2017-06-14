72eb598141e432b3e464e35e832ad783a4c0dd00
Kiko Beats <josefrancisco.verdu@gmail.com>
2017-06-14 15:45:36 +0200

    Fix mobile scroll

--- src/app/HomeHero/index.js
+++ src/app/HomeHero/index.js
@@ -1,9 +1,9 @@
 import React from 'react'
 
-const navLinkStyle = 'home-header__navbar-link no-underline flex ttu b items-center justify-center flex-row h-100 ph3 f6 blue-300'
+const navLinkStyle = 'no-underline flex ttu b items-center justify-center flex-row h-100 ph3 f6 blue-300'
 
 export default () => (
-  <article className='home w-100 bg-blue-500 white pt4'>
+  <article className='w-100 bg-blue-500 white pt4'>
     <div className='pb4 ph3 ph7-ns tc lh-copy'>
       <p className='f2 avenir fw3 ma0 pv2'>
         windsurf marketplace 💨
@@ -13,39 +13,39 @@ export default () => (
       </p>
     </div>
 
-    <nav className='home-header__navbar flex justify-center bg-white card-shadow'>
-      <ul className='home-header__navbar-list list flex'>
-        <li className='home-header__navbar-item'>
+    <nav className='flex justify-center bg-white card-shadow overflow-y-visible overflow-x-scroll'>
+      <ul className='list flex'>
+        <li className=''>
           <a
             className={navLinkStyle}
             target='_blank'
             href='/'>Sails</a>
         </li>
-        <li className='home-header__navbar-item'>
+        <li className=''>
           <a
             className={navLinkStyle}
             target='_blank'
             href='/'>Boards</a>
         </li>
-        <li className='home-header__navbar-item'>
+        <li className=''>
           <a
             className={navLinkStyle}
             target='_blank'
             href='/'>Masts</a>
         </li>
-        <li className='home-header__navbar-item'>
+        <li className=''>
           <a
             className={navLinkStyle}
             target='_blank'
             href='/'>Fins</a>
         </li>
-        <li className='home-header__navbar-item'>
+        <li className=''>
           <a
             className={navLinkStyle}
             target='_blank'
             href='/'>Booms</a>
         </li>
-        <li className='home-header__navbar-item'>
+        <li className=''>
           <a
             className={navLinkStyle}
             target='_blank'
