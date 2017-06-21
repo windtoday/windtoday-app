18968f017bc70d5790a5fd6135b292b4ae813504
Kiko Beats <josefrancisco.verdu@gmail.com>
2017-06-21 10:55:34 +0200

    Adjust card background

--- src/app/ProductDivImage/index.js
+++ src/app/ProductDivImage/index.js
@@ -5,8 +5,8 @@ export default ({product, className, width, children}) => {
   const src = getImageUrl(product, width)
 
   const linearGradient = `linear-gradient(
-    rgba(255, 255, 255, 0.1) 0%,
-    rgba(0, 0, 0, 0.6) 100%
+    rgba(0, 0, 0, 0.2) 20%,
+    rgba(0, 0, 0, 0.7) 100%
   )`
 
   const style = {
