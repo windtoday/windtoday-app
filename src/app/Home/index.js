1c8ef34ce4a9d06a9b372f7b4fbb145ce50a6c93
Kiko Beats <josefrancisco.verdu@gmail.com>
2017-06-22 18:00:58 +0200

    Clear search event

--- src/app/Home/index.js
+++ src/app/Home/index.js
@@ -1,18 +1,29 @@
+import createClass from 'create-react-class'
 import React from 'react'
 
 import HomeProducts from '../HomeProducts'
 import Footer from '../Footer'
 import HomeHero from '../HomeHero'
 
-export default ({get}) => (
-  <main>
-    <HomeHero />
+export default createClass({
+  componentWillMount () {
+    this.props.set('onSearchClear', () => {})
+  },
 
-    <HomeProducts
-      filters=''
-      hitsPerPage={get('hitsPerPage')}
-    />
+  render () {
+    const {get} = this.props
 
-    <Footer />
-  </main>
-)
+    return (
+      <main>
+        <HomeHero />
+
+        <HomeProducts
+          filters=''
+          hitsPerPage={get('hitsPerPage')}
+        />
+
+        <Footer />
+      </main>
+    )
+  }
+})
