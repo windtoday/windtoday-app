3780c0418a959a18e6a3470fea7349840b750aa9
Kiko Beats <josefrancisco.verdu@gmail.com>
2017-08-02 17:06:20 +0200

    Add price score

--- components/Hit/index.js
+++ components/Hit/index.js
@@ -1,17 +1,30 @@
 import PropTypes from 'prop-types'
 import styled from 'styled-components'
+import {ChevronDown} from 'react-feather'
 
-import { Text, Flex, Avatar, Divider, Box, BackgroundImage } from 'rebass'
+import { Text, Flex, Divider, Box, BackgroundImage } from 'rebass'
+import ProgressArc from 'progress-arc-component'
 import getImageUrl from 'util/get-image-url'
+import ColourMeLife from 'colour-me-life'
+import TimeAgo from 'react-timeago'
 
-const AVATAR_SIZE = 40
+const formatter = (value, unit, suffix) => {
+  if (value !== 1) unit += 's'
+  return `${value}${unit.charAt(0)}`
+}
+
+const gradient = new ColourMeLife()
+  .setSpectrum('#6e4b46', '#9e6b64', '#1ac391')
+  .setNumberRange(0, 100)
+
+const AVATAR_SIZE = '40px'
 
 const Content = styled(Box)`
 margin: 0 .32813rem;
 `
 
 const SecondaryContent = styled(Content)`
-min-width: ${AVATAR_SIZE}px;
+min-width: ${AVATAR_SIZE};
 max-width: 3.28125rem;
 flex-grow: 1;
 `
@@ -19,22 +32,39 @@ flex-grow: 1;
 const PrimaryContent = styled(Content)`
 flex-grow: 7;
 `
+const CustomProgressArc = styled(ProgressArc)`
+margin: 0 !important;
+height: ${AVATAR_SIZE} !important;
+text {
+  font-size: 70px !important;
+}
+`
+const getColor = value => `#${gradient.colourAt(value)}`
+
 const HitComponent = ({ hit }) =>
   <Box>
     <Box py={3} px={2}>
       <Flex direction='row'>
         <SecondaryContent>
-          <Avatar
-            size={40}
-            src={`/static/img/provider/${hit.provider}.jpg`}
+          <CustomProgressArc
+            value={hit.priceScore}
+            unit=''
+            arcColor={getColor(hit.priceScore)}
+            textColor='black'
+            rounded
           />
         </SecondaryContent>
         <PrimaryContent>
-          <Box>
-            <Text is='span' bold>{hit.provider}</Text>{' · '}
-            <Text is='span' color='gray7'>€{hit.price}</Text>{' · '}
-            <Text is='span' color='gray7'>5min</Text>
-          </Box>
+          <Flex direction='row' mb={1} justify='space-between' align='center'>
+            <Box>
+              <Text is='span' bold>{hit.provider}</Text>{' · '}
+              <Text is='span' color='gray7'>€{hit.price}</Text>{' · '}
+              <Text is='span' color='gray7'><TimeAgo formatter={formatter} date={hit.timestamp} /></Text>
+            </Box>
+            <Box>
+              <ChevronDown color='gray' />
+            </Box>
+          </Flex>
           <Box>
             <Text>{hit.title}</Text>
           </Box>
