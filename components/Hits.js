a438d3b78956494ffa8fab7043483bc17357ae0c
Kiko Beats <josefrancisco.verdu@gmail.com>
2017-08-03 20:41:08 +0200

    Setup Infinite Hits Component

--- components/Hits.js
+++ components/Hits.js
@@ -1,4 +1,41 @@
-import Hit from './Hit'
-import { Hits } from 'react-instantsearch/dom'
+import { connectInfiniteHits } from 'react-instantsearch/connectors'
+import InfiniteScroll from 'react-infinite-scroll-component'
+import styled from 'styled-components'
+import PropTypes from 'prop-types'
+import { media } from 'util/style'
+import { Component } from 'react'
+import { Box } from 'rebass'
 
-export default () => <Hits hitComponent={Hit} style={{ width: '100%' }} />
+const CustomBox = styled(Box)`
+${media.large`
+  max-width: 600px;
+  margin: 0 auto;
+`}
+`
+
+class InfiniteHits extends Component {
+  render () {
+    const { hitComponent: ItemComponent, hits, hasMore, refine } = this.props
+    const renderedHits = hits.map(hit =>
+      <ItemComponent key={hit.objectID} hit={hit} />
+    )
+
+    return (
+      <CustomBox>
+        <InfiniteScroll next={refine} hasMore={hasMore} scrollThreshold={0.4}>
+          {renderedHits}
+        </InfiniteScroll>
+      </CustomBox>
+    )
+  }
+}
+
+InfiniteHits.propTypes = {
+  hits: PropTypes.array,
+  hitComponent: PropTypes.oneOfType([PropTypes.string, PropTypes.func])
+    .isRequired,
+  hasMore: PropTypes.bool.isRequired,
+  refine: PropTypes.func.isRequired
+}
+
+export default connectInfiniteHits(InfiniteHits)
