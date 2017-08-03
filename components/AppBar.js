db5b03fdaa3352283286fe17150da7ca2de80562
Kiko Beats <josefrancisco.verdu@gmail.com>
2017-08-03 11:32:31 +0200

    Remove unnecessasry deps

--- components/AppBar.js
+++ components/AppBar.js
@@ -1,15 +1,16 @@
-import {Box, Fixed, Heading, Drawer, Toolbar, NavLink} from 'rebass'
-import {SearchBox} from 'react-instantsearch/dom'
-import {Menu, Search, X} from 'react-feather'
-import {createProvider} from 'refunk'
+import { Box, Fixed, Heading, Drawer, Toolbar, NavLink } from 'rebass'
+import { SearchBox } from 'react-instantsearch/dom'
+import { Menu, Search, X } from 'react-feather'
+import { createProvider } from 'refunk'
 
-const hoc = createProvider({isDrawerOpen: false, isSearchOpen: false})
-const toggleDrawer = state => ({isDrawerOpen: !state.isDrawerOpen})
-const toggleSearch = state => ({isSearchOpen: !state.isSearchOpen})
+const hoc = createProvider({ isDrawerOpen: false, isSearchOpen: false })
+const toggleDrawer = state => ({ isDrawerOpen: !state.isDrawerOpen })
+const toggleSearch = state => ({ isSearchOpen: !state.isSearchOpen })
 
-const AppBar = hoc(({isDrawerOpen, isSearchOpen, update}) =>
+const AppBar = hoc(({ isDrawerOpen, isSearchOpen, update }) =>
   <div>
-    {isDrawerOpen && <Fixed top right bottom left onClick={e => update(toggleDrawer)} />}
+    {isDrawerOpen &&
+      <Fixed top right bottom left onClick={e => update(toggleDrawer)} />}
 
     <Drawer open={isDrawerOpen} color='white' bg='gray9'>
       <Heading>Hello</Heading>
@@ -23,8 +24,10 @@ const AppBar = hoc(({isDrawerOpen, isSearchOpen, update}) =>
       <Box mx='auto'>
         {!isSearchOpen
           ? <NavLink mx='auto' children='WINDTODAY' />
-          : <SearchBox translations={{placeholder: 'What are you looking for?'}} autoFocus />
-        }
+          : <SearchBox
+              translations={{ placeholder: 'What are you looking for?' }}
+              autoFocus
+            />}
       </Box>
 
       <NavLink>
